4c543ded28a7ee62f20f3ef01cc342263b6bed63
leandro <leandrofagner.part@gmail.com>
2020-11-14 15:43:43 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,154 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Site pessoal - Leandro Almeida - Suporte TI e Desenvolvimento Web.">    <title>Leandro Almeida</title>    
+    <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.1/build/pure-min.css" integrity="sha384-" crossorigin="anonymous">
+    
+    <!--[if lte IE 8]>
+        <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.1/build/grids-responsive-old-ie-min.css">
+    <![endif]-->
+    <!--[if gt IE 8]><!-->
+        <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.1/build/grids-responsive-min.css">
+    <!--<![endif]-->
+    
+    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+    
+        <!--[if lte IE 8]>
+            <link rel="stylesheet" href="css/layouts/marketing-old-ie.css">
+        <![endif]-->
+        <!--[if gt IE 8]><!-->
+            <link rel="stylesheet" href="css/layouts/marketing.css">
+        <!--<![endif]-->
+</head>
+<body>
+
+<div class="header">
+    <div class="home-menu pure-menu pure-menu-horizontal pure-menu-fixed">
+        <a class="pure-menu-heading" href="">Leandro Almeida</a>
+<!--
+        <ul class="pure-menu-list">
+            <li class="pure-menu-item pure-menu-selected"><a href="#" class="pure-menu-link">Início</a></li>
+            <li class="pure-menu-item"><a href="#" class="pure-menu-link">Sobre</a></li>
+            <li class="pure-menu-item"><a href="#" class="pure-menu-link">Acesso</a></li>
+        </ul>
+-->
+    </div>
+</div>
+
+<div class="splash-container">
+    <div class="splash">
+        <h1 class="splash-head">Leandro Almeida</h1>
+        <p class="splash-subhead">
+            Suporte TI e Análise e Desenvolvimento de Sistemas.
+        </p>
+        <p>
+            <a href="https://github.com/leandroadev" class="pure-button pure-button-primary">GitHub</a>
+        </p>
+    </div>
+</div>
+
+<div class="content-wrapper">
+    <div class="content">
+        <h2 class="content-head is-center">Serviços</h2>
+
+        <div class="pure-g">
+            <div class="l-box pure-u-1 pure-u-md-1-2 pure-u-lg-1-4">
+
+                <h3 class="content-subhead">
+                    <i class="fa fa-check-square-o"></i>
+                    Moodle
+                </h3>
+                <p>
+                    Instalação e suporte ao Ambiente Virtual de Apredizagem - Moodle
+                </p>
+            </div>
+            <div class="l-box pure-u-1 pure-u-md-1-2 pure-u-lg-1-4">
+                <h3 class="content-subhead">
+                    <i class="fa fa-check-square-o"></i>
+                    Computadores
+                </h3>
+                <p>
+                    Formatação de computadores.
+                </p>
+            </div>
+            <div class="l-box pure-u-1 pure-u-md-1-2 pure-u-lg-1-4">
+                <h3 class="content-subhead">
+                    <i class="fa fa-check-square-o"></i>
+                    Suporte TI
+                </h3>
+                <p>
+                    Suporte a usuários de informática.
+                </p>
+            </div>
+            <div class="l-box pure-u-1 pure-u-md-1-2 pure-u-lg-1-4">
+                <h3 class="content-subhead">
+                    <i class="fa fa-check-square-o"></i>
+                    Desenvolvimento
+                </h3>
+                <p>
+                    Desenvolvimento de sites com as tecnologias HTML5, CSS3 e JavaScript.
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <div class="ribbon l-box-lrg pure-g">
+        <div class="l-box-lrg is-center pure-u-1 pure-u-md-1-2 pure-u-lg-2-5">
+            <img width="300" alt="File Icons" class="pure-img-responsive" src="img/common/file-icons.png">
+        </div>
+        <div class="pure-u-1 pure-u-md-1-2 pure-u-lg-3-5">
+
+            <h2 class="content-head content-head-ribbon">Experiência Profissional.</h2>
+
+            <p>Mais de 12 anos de atuação na área Técnica e de Supervisão, 
+                com experiência na coordenação, planejamento e execução 
+                de atividades tecnológicas da educação, na  
+                configuração e manutenção de computadores, 
+                impressoras etc, no atendimento as demandas relacionadas a 
+                dúvidas técnicas de usuários de informática, no desenvolvimento 
+                e gerenciamento do Moodle - Ambiente Virtual de Aprendizagem (AVA). 
+                Assim como, na implementação de melhorias na navegabilidade de usuários do 
+                Moodle (AVA).
+            </p>
+
+            <p>Atuação na área Técnica, com experiência em Suporte Tecnológico e Desenvolvimento 
+                de Sistemas, experiência com monitoria de informática na educação infantil. 
+                Atuação na área de Desenvolvimento e Suporte Técnico a Sistemas de Gestão 
+                de Aprendizagem Implantados em empresas privadas.
+            </p>
+        </div>
+    </div>
+
+    <div class="content">
+        <h2 class="content-head is-center">Contato</h2>
+
+        <div class="pure-g">
+
+            <div class="l-box-lrg pure-u-1">
+                <h4>Entre em contato!</h4>
+                <p>
+                    Para melhor atendê-los, por favor, entrem em contato pelo endereço de e-mail: leandrofagner.part@gmail.com
+                </p>
+<!--
+                <h4>More Information</h4>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua.
+                </p>
+-->
+            </div>
+        </div>
+
+    </div>
+
+    <div class="footer l-box is-center">
+        Copyright &copy <?php echo date("Y"); ?> Leandro Almeida.
+    </div>
+
+</div>
+
+
+</body>
+</html>
